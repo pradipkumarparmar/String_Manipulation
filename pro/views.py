2a51b0f79aa76399d6119c6141777f5fa6c7635d
pradipkumarparmar <parmarpradip0908@gmail.com>
2020-08-18 08:21:22 +0530

    string manipulation django

---
+++ pro/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    reversed_sentence = None
+    if request.method == "POST":
+        name = request.POST["pradip"]
+       #print(name)
+        word_list = name.split('.')
+
+        print(word_list)
+
+        #rev= word_list[:: 2]
+
+        reversed_list = word_list[:: -1]
+
+        reversed_sentence = " ".join(reversed_list)
+    
+        print(reversed_sentence)
+
+
+    return render(request,'index.html',{'getvalue':reversed_sentence})
+
+
+    
